d3fe38ee60c19e04e239a7f7a4c9741d089e2a88
Logan Morazain <lxmorazain@gmail.com>
2024-12-13 06:43:47 +0000

    Implemented Wallhugging Navigator

--- compass.py
+++ compass.py
@@ -1,16 +1,42 @@
 from constants import *
+import time
 import maze
-
-class Stack():
-
-    def __init__(self):
-        pass
 
 class Navigator():
 
     def __init__(self, maze):
-        pass
+        self.maze = maze
+        self.facing = "up"
 
-    def dfs(self):
-        pass
+    def wallfollow(self):
+        maze = self.maze
+        if self.facing == "left":
+            if maze.check_down():
+                self.facing="down"
+                maze.move_down()
+            else:
+                self.facing="down"
+
+        if self.facing == "down":
+            if maze.check_right():
+                self.facing="right"
+                maze.move_right()
+            else:
+                self.facing="right"
+
+        if self.facing == "right":
+            if maze.check_up():
+                self.facing="up"
+                maze.move_up()
+            else:
+                self.facing="up"
+
+        if self.facing == "up":
+            if maze.check_left():
+                self.facing="left"
+                maze.move_left()
+            else:
+                self.facing="left"
+
+        
 
